0342d149028624638c26824039427a6a3c972c9a
Mark Ustby <ustbymark@gmail.com>
2022-08-30 08:23:50 -0500

    stuff

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,15 +1,18 @@
-import { Query } from "@apollo/react-components";
 import { gql } from "apollo-boost";
 
 const GET_ME = gql`
-  {
-    #call the me query in the resolvers file while passing the token
-    me(token) {
-        id
-        username
-        email
-        bookCount
-        SavedBooks
-    }
+    me {
+      id
+      username
+      email
+      bookCount
+      SavedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
   }
 `;
